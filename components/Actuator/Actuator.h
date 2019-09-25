f33c9bab11a8702dd366055279811b9cb24269f3
yonycherkos <yonycherkos@gmail.com>
2019-09-25 18:38:14 +0300

    adding actuator header and implementation files.

--- components/Actuator/Actuator.h
+++ components/Actuator/Actuator.h
@@ -1,15 +1,20 @@
-#ifndef ACTUATOR_H
-#define ACTUATOR_H
 
-
-namespace components{
-
-class Actuator
+class Actuators
 {
-    public:
-        Actuator();
-        ~Actuator();
-};
-     
-} 
-#endif;
+private:
+  pair<int, int> agent_location;
+  string agent_direction;
+
+public:
+  Actuators(pair<int, int> agent_location, string agent_direction);
+
+  pair<int, int> get_agent_location();
+  void set_agent_location(pair<int, int> agent_location);
+
+  pair<int, int> get_agent_direction();
+  void set_agent_direction(pair<int, int> agent_location);
+
+  room_direction_from_agent(pair<int, int> room_location);
+  void move_to(pair<int, int> room_location);
+  void shoot_at(pair<int, int> room_location);
+}
